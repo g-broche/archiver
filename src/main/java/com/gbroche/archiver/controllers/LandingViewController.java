9c25de9836623f57840a34fedb16f6752d92a4dc
g-broche <gregory.broche@yahoo.fr>
2026-06-04 18:23:55 +0200

    working on user interactions with directories

--- src/main/java/com/gbroche/archiver/controllers/LandingViewController.java
+++ src/main/java/com/gbroche/archiver/controllers/LandingViewController.java
@@ -1,13 +1,15 @@
 package com.gbroche.archiver.controllers;
 
+import com.gbroche.archiver.utils.FileUtils;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
 
 public class LandingViewController {
     @FXML
@@ -17,18 +19,32 @@ public class LandingViewController {
     @FXML
     private Button extractButton;
     @FXML
-    private Label selectedFilePath;
+    private CheckBox addSubDirCheckbox;
+    @FXML
+    private Label selectedArchivePathLabel;
     @FXML
     private TextField extractionPathField;
     @FXML
     private TextField archivePasswordField;
 
-    private File selectedArchive;
     private Stage stage;
+    private boolean mustIncludeExtractionSubDir = true;
+    private File selectedArchive;
+    private File extractionDirectory;
 
     @FXML
     public void initialize() {
         extractButton.setDisable(true);
+        addSubDirCheckbox.setSelected(true);
+
+        extractionPathField.textProperty().addListener((observable, oldValue, newValue) -> {
+            try {
+                Path path = Path.of(newValue).toAbsolutePath().normalize();
+                extractionDirectory = path.toFile();
+            } catch (InvalidPathException e) {
+                new Alert(Alert.AlertType.INFORMATION, "invalid character entered").showAndWait();
+            }
+        });
     }
 
     public void setStage(Stage stage) {
@@ -37,14 +53,82 @@ public class LandingViewController {
 
     @FXML
     private void selectArchive(){
-        new Alert(Alert.AlertType.INFORMATION, "select archive triggered").showAndWait();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select archive to extract");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("All Archives", "*.zip", "*.rar", "*.7z"),
+                new FileChooser.ExtensionFilter("ZIP files", "*.zip"),
+                new FileChooser.ExtensionFilter("RAR files", "*.rar"),
+                new FileChooser.ExtensionFilter("7z files", "*.7z")
+        );
+        File file = fileChooser.showOpenDialog(stage);
+        if (file != null) {
+            selectedArchive = file;
+            selectedArchivePathLabel.setText(file.getAbsolutePath());
+            extractionDirectory = mustIncludeExtractionSubDir
+                ? new File(selectedArchive.getParentFile(), FileUtils.getFileNameWithoutExtension(selectedArchive))
+                : selectedArchive.getParentFile();
+            extractionPathField.setText(extractionDirectory.getAbsolutePath());
+
+            extractButton.setDisable(false);
+        } else {
+            System.out.println("no file selected");
+        }
     }
+
     @FXML
     private void selectDestinationOfExtraction(){
-        new Alert(Alert.AlertType.INFORMATION, "select destination triggered").showAndWait();
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Select directory to extract to");
+        if (extractionDirectory != null) {
+            directoryChooser.setInitialDirectory(extractionDirectory);
+        }
+        File dir = directoryChooser.showDialog(stage);
+        if (dir != null) {
+            extractionDirectory = dir;
+            extractionPathField.setText(extractionDirectory.getAbsolutePath());
+            extractButton.setDisable(false);
+            System.out.println("extraction path: "+extractionDirectory.getAbsolutePath());
+
+        } else {
+            System.out.println("no extraction directory selected");
+        }
     }
+
+    @FXML
+    private void toggleExtractionSubDir(){
+        mustIncludeExtractionSubDir = addSubDirCheckbox.isSelected();
+        appendSubDirIfRequired();
+    }
+
     @FXML
     private void extractArchive(){
         new Alert(Alert.AlertType.INFORMATION, "extraction triggered").showAndWait();
     }
+
+    private void addExtractionSubDir(){
+        if (selectedArchive == null || extractionDirectory == null) {
+            return;
+        }
+        boolean isArchiveInsideExtractionDirectory = FileUtils.isDirectoryDirectParentOfFile(extractionDirectory, selectedArchive);
+        if (isArchiveInsideExtractionDirectory){
+            File parentFile = selectedArchive.getParentFile();
+            String archiveName = FileUtils.getFileNameWithoutExtension(selectedArchive);
+            extractionDirectory = new File(parentFile, archiveName);
+            return;
+        }
+        String currentSelectedExtractionDir = extractionDirectory.getAbsolutePath();
+        String archiveName = FileUtils.getFileNameWithoutExtension(selectedArchive);
+        extractionDirectory = new File(currentSelectedExtractionDir, archiveName);
+    }
+
+    private void appendSubDirIfRequired(){
+        if (mustIncludeExtractionSubDir){
+            addExtractionSubDir();
+        } else {
+            new Alert(Alert.AlertType.INFORMATION, "Must implement removal of archive name").showAndWait();
+        }
+    }
 }
+
+
---
+++ src/main/java/com/gbroche/archiver/utils/FileUtils.java
@@ -0,0 +1,27 @@
+package com.gbroche.archiver.utils;
+
+import java.io.File;
+import java.nio.file.Path;
+
+public class FileUtils {
+
+    static public String getFileExtension(File file) {
+        String name = file.getName();
+        int extensionIndex = name.lastIndexOf('.');
+        return extensionIndex >= 0 ? name.substring(extensionIndex + 1) : "";
+    }
+
+    static public String getFileNameWithoutExtension(File file) {
+        String name = file.getName();
+        int extensionIndex = name.lastIndexOf('.');
+        return extensionIndex >= 0 ? name.substring(0, extensionIndex) : name;
+    }
+
+    static public boolean isDirectoryDirectParentOfFile(File candidateParent, File candidateChild){
+        Path childPath = candidateChild.toPath().toAbsolutePath().normalize();
+        Path directParent = childPath.getParent();
+        if (directParent == null) return false;
+        Path parentPath = candidateParent.toPath().toAbsolutePath().normalize();
+        return directParent.equals(parentPath);
+    }
+}
